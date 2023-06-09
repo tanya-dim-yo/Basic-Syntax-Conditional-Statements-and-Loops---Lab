4cb3e75dcbd3804cc086763d2fad53d86b50f980
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-10 02:29:02 +0300

    Add files via upload

---
+++ 06. Foreign Languages/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+string countryName = Console.ReadLine();
+
+switch (countryName)
+{
+    case ("England"):
+    case ("USA"):
+        Console.WriteLine($"English");
+        break;
+    case ("Spain"):
+    case ("Argentina"):
+    case ("Mexico"):
+        Console.WriteLine($"Spanish");
+        break;
+    default:
+        Console.WriteLine($"unknown");
+        break;
+}
