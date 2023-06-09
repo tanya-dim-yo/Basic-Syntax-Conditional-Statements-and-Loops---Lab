8c6e32b0d7db9122130c272dd8eb99371f1afd96
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-09 05:15:47 +0300

    Add files via upload

---
+++ 04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,15 @@
+﻿int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine()) + 30;
+
+if (minutes >= 60)
+{
+    hours += 1;
+    minutes -= 60;
+}
+
+if (hours >= 24)
+{
+    hours = 0;
+}
+
+Console.WriteLine($"{hours}:{minutes:D2}");
