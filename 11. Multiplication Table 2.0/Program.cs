cb50951952132a37b3988507236f559c072884db
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-11 04:27:07 +0300

    Add files via upload

---
+++ 11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+int multiplier = int.Parse(Console.ReadLine());
+
+if (multiplier <= 10)
+{
+    for (int i = multiplier; i <= 10; i++)
+    {
+        Console.WriteLine($"{n} X {i} = {n * i}");
+    }
+}
+else if (multiplier > 10)
+{
+    Console.WriteLine($"{n} X {multiplier} = {n * multiplier}");
+}
