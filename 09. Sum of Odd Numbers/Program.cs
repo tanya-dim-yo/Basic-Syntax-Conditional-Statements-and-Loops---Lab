d3b5f8b532c284aaecbd815b36c0408387263e74
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-11 04:25:32 +0300

    Add files via upload

---
+++ 09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+int a;
+int sum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    a = 1 + i * 2;
+    Console.WriteLine(" " + a);
+    sum += a;
+}
+
+Console.WriteLine($"Sum: {sum}");
