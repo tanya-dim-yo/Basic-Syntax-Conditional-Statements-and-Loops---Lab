0a9628127c47099e90d5cb0c36f81910e4afd379
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-11 04:27:45 +0300

    Add files via upload

---
+++ 12. Even Number/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+while (n % 2 != 0)
+{
+    Console.WriteLine($"Please write an even number.");
+
+    n = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"The number is: {Math.Abs(n)}");
